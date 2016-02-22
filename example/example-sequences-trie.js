300e96699e48143c8177f4f3ee500ed9116e0195
Jason Walsh <j@wal.sh>
2016-02-21 19:04:47 -0500

    Reviewing examples
    Provide a mechanism for lookup of the number of times a sequence is
    seen.

---
+++ example/example-sequences-trie.js
@@ -0,0 +1,22 @@
+var IngressGlyphs = require('../dist/ingress');
+var fs = require('fs');
+var sequences = IngressGlyphs.ingress.sequences;
+
+var trie = {};
+for (var i = 0; i < sequences.length; i++) {
+  var sequence = sequences[i],
+      cur = trie;
+  for (var j = 0; j < sequence.length; j++) {
+    var glyph = sequence[j], pos = cur[glyph];
+    if (pos == null) {
+      cur = cur[glyph] = j === sequence.length - 1 ? 0 : {};
+    } else if (pos === 0) {
+      cur = cur[glyph] = { $: 0 };
+    } else {
+      cur = cur[glyph];
+    }
+  }
+}
+
+console.log('public/sequences.json');
+fs.writeFile('public/sequences.json', JSON.stringify(trie, null, '  '));
